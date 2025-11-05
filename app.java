a6336f9cfa8a0626a57c6b96b5377c44bc0c9334
chethanteja-kumar <chethantejakumar66@gmail.com>
2025-11-05 21:40:18 +0530

    app java second commit

--- app.java
+++ app.java
@@ -3,3 +3,19 @@ public class HelloWorld {
 		            System.out.println("Hello, World!");
 			        }
 }
+//dev2
+public class VariablesExample {
+    public static void main(String[] args) {
+        int age = 30; // Integer variable
+        double price = 19.99; // Double-precision floating-point variable
+        char initial = 'J'; // Character variable
+        boolean isActive = true; // Boolean variable
+        String name = "John Doe"; // String variable
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Price: " + price);
+        System.out.println("Initial: " + initial);
+        System.out.println("Is Active: " + isActive);
+    }
+}
